a8eccc2df132339f26e7d49154b05cc4ea573ced
Damian Lubera <dmlubera@gmail.com>
2021-09-12 21:38:47 +0200

    Added user's registration event handling

---
+++ src/Modules/Users/YetAnotherECommerce.Modules.Users.Core/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace YetAnotherECommerce.Modules.Users.Core.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        public User(Guid id, string email, string password)
+        {
+            Id = id;
+            Email = email;
+            Password = password;
+        }
+    }
+}
---
+++ src/Modules/Users/YetAnotherECommerce.Modules.Users.Core/Events/External/Handlers/UserRegisteredHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using YetAnotherECommerce.Modules.Identity.Messages.Events;
+using YetAnotherECommerce.Modules.Users.Core.Entities;
+using YetAnotherECommerce.Modules.Users.Core.Repositories;
+using YetAnotherECommerce.Shared.Abstractions.Events;
+
+namespace YetAnotherECommerce.Modules.Users.Core.Events.External.Handlers
+{
+
+    internal class UserRegisteredHandler : IEventHandler<UserRegistered>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserRegisteredHandler(IUserRepository userRepository)
+            => _userRepository = userRepository;
+
+        public async Task HandleAsync(UserRegistered @event)
+        {
+            var user = new User(@event.Id, @event.Email, @event.Password);
+
+            await _userRepository.AddAsync(user);
+        }
+    }
+}
