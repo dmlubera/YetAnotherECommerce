f35f03e07693dfdef48b521a46455763f01b0d3a
Damian Lubera <dmlubera@gmail.com>
2021-10-20 21:29:08 +0200

    Event published when order created

--- src/Modules/Orders/YetAnotherECommerce.Modules.Orders.Core/Events/External/Handlers/OrderPlacedHandler.cs
+++ src/Modules/Orders/YetAnotherECommerce.Modules.Orders.Core/Events/External/Handlers/OrderPlacedHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using YetAnotherECommerce.Modules.Carts.Messages.Events;
 using YetAnotherECommerce.Modules.Orders.Core.Entities;
@@ -10,22 +11,28 @@ namespace YetAnotherECommerce.Modules.Orders.Core.Events.External.Handlers
     public class OrderPlacedHandler : IEventHandler<OrderPlaced>
     {
         private readonly IOrderRepository _orderRepository;
+        private readonly IEventDispatcher _eventDispatcher;
 
-        public OrderPlacedHandler(IOrderRepository orderRepository)
+        public OrderPlacedHandler(IOrderRepository orderRepository, IEventDispatcher eventDispatcher)
         {
             _orderRepository = orderRepository;
+            _eventDispatcher = eventDispatcher;
         }
 
         public async Task HandleAsync(OrderPlaced @event)
         {
             var orderItems = new List<OrderItem>();
+            var productsWithQuantity = new Dictionary<Guid, int>();
             foreach(var item in @event.Products)
             {
                 orderItems.Add(new OrderItem(item.ProductId, item.Name, item.UnitPrice, item.Quanitity));
+                productsWithQuantity.Add(item.ProductId, item.Quanitity);
             }
             var order = new Order(@event.CustomerId, orderItems);
 
             await _orderRepository.AddAsync(order);
+
+            await _eventDispatcher.PublishAsync(new OrderCreated(order.Id, productsWithQuantity));
         }
     }
 }
