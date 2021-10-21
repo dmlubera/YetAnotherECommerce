ed57971dedfc5b67d5b35e7a8db319811c19b9be
Damian Lubera <dmlubera@gmail.com>
2021-10-21 20:33:38 +0200

    Added products availability after order creation

---
+++ src/Modules/Products/YetAnotherECommerce.Modules.Products.Core/Events/External/Handlers/OrderCreatedHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using YetAnotherECommerce.Modules.Orders.Messages.Events;
+using YetAnotherECommerce.Modules.Products.Core.Repositories;
+using YetAnotherECommerce.Modules.Products.Messages.Events;
+using YetAnotherECommerce.Shared.Abstractions.Events;
+
+namespace YetAnotherECommerce.Modules.Products.Core.Events.External.Handlers
+{
+    public class OrderCreatedHandler : IEventHandler<OrderCreated>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IEventDispatcher _eventDispatcher;
+
+        public OrderCreatedHandler(IProductRepository productRepository, IEventDispatcher eventDispatcher)
+        {
+            _productRepository = productRepository;
+            _eventDispatcher = eventDispatcher;
+        }
+
+        public async Task HandleAsync(OrderCreated @event)
+        {
+            foreach(var orderedProduct in @event.Products)
+            {
+                var product = await _productRepository.GetByIdAsync(orderedProduct.Key);
+                if (product is null || product.Quantity < orderedProduct.Value)
+                {
+                    await _eventDispatcher.PublishAsync(new OrderRejected(@event.OrderId));
+                }
+                else
+                {
+                    product.UpdateQuantity(product.Quantity - orderedProduct.Value);
+                    await _productRepository.UpdateAsync(product);
+                }
+            }
+
+            await _eventDispatcher.PublishAsync(new OrderAccepted(@event.OrderId));
+        }
+    }
+}
---
+++ src/Modules/Products/YetAnotherECommerce.Modules.Products.Messages/Events/OrderAccepted.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using YetAnotherECommerce.Shared.Abstractions.Events;
+
+namespace YetAnotherECommerce.Modules.Products.Messages.Events
+{
+    public class OrderAccepted : IEvent
+    {
+        public Guid OrderId { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public OrderAccepted(Guid orderId)
+        {
+            OrderId = orderId;
+            DateTime = DateTime.UtcNow;
+        }
+    }
+}
