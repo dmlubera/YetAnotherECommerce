36c9af9f4e018d2dc38be721de277ac184a61079
Damian Lubera <dmlubera@gmail.com>
2021-10-24 17:07:27 +0200

    Added publishing event when user completes registration

--- src/Modules/Users/YetAnotherECommerce.Modules.Users.Core/Commands/CompleteRegistrationCommandHandler.cs
+++ src/Modules/Users/YetAnotherECommerce.Modules.Users.Core/Commands/CompleteRegistrationCommandHandler.cs
@@ -1,15 +1,21 @@
 ﻿using System.Threading.Tasks;
 using YetAnotherECommerce.Modules.Users.Core.Repositories;
+using YetAnotherECommerce.Modules.Users.Messages.Events;
 using YetAnotherECommerce.Shared.Abstractions.Commands;
+using YetAnotherECommerce.Shared.Abstractions.Events;
 
 namespace YetAnotherECommerce.Modules.Users.Core.Commands
 {
     public class CompleteRegistrationCommandHandler : ICommandHandler<CompleteRegistrationCommand>
     {
         private readonly IUserRepository _userRepository;
+        private readonly IEventDispatcher _eventDispatcher;
 
-        public CompleteRegistrationCommandHandler(IUserRepository userRepository)
-            => _userRepository = userRepository;
+        public CompleteRegistrationCommandHandler(IUserRepository userRepository, IEventDispatcher eventDispatcher)
+        {
+            _userRepository = userRepository;
+            _eventDispatcher = eventDispatcher;
+        }
 
         public async Task HandleAsync(CompleteRegistrationCommand command)
         {
@@ -20,6 +26,9 @@ namespace YetAnotherECommerce.Modules.Users.Core.Commands
             user.UpdateAddress(command.Street, command.City, command.ZipCode, command.Country);
 
             await _userRepository.UpdateAsync(user);
+
+            await _eventDispatcher.PublishAsync(
+                new RegistrationCompleted(user.Id, user.FirstName, user.LastName, user.Email, user.Address.ToString()));
         }
     }
 }
