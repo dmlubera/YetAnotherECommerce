91a7d206138220790bc46a3d336884c50f0e5380
Damian Lubera <dmlubera@gmail.com>
2021-10-25 20:32:56 +0200

    Handled invalid states when placing order

--- src/Modules/Carts/YetAnotherECommerce.Modules.Carts.Core/Services/CartService.cs
+++ src/Modules/Carts/YetAnotherECommerce.Modules.Carts.Core/Services/CartService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using YetAnotherECommerce.Modules.Carts.Core.Entities;
+using YetAnotherECommerce.Modules.Carts.Core.Exceptions;
 using YetAnotherECommerce.Modules.Carts.Messages.Events;
 using YetAnotherECommerce.Modules.Carts.Messages.Model;
 using YetAnotherECommerce.Shared.Abstractions.Cache;
@@ -26,9 +27,14 @@ namespace YetAnotherECommerce.Modules.Carts.Core.Services
         {
             var cart = _cache.Get<Cart>($"{userId}-cart");
 
+            if (cart.Items.Count == 0)
+                throw new CannotCreateOrderFromEmptyCartException();
+
             var productDtos = new List<ProductDto>();
             foreach(var item in cart.Items)
             {
+                if (item.Quantity == 0)
+                    throw new CannotOrderProductInZeroQuantityException();
                 productDtos.Add(new ProductDto(item.ProductId, item.Name, item.UnitPrice, item.Quantity));
             }
 
