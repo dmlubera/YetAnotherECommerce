593ffc573e351668f71c00ef00e6830d0ad0a558
Damian Lubera <dmlubera@gmail.com>
2022-02-26 19:24:28 +0100

    Added handling for updating price

---
+++ src/Modules/Products/YetAnotherECommerce.Modules.Products.Core/Commands/UpdatePriceCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using YetAnotherECommerce.Modules.Products.Core.Events;
+using YetAnotherECommerce.Modules.Products.Core.Exceptions;
+using YetAnotherECommerce.Modules.Products.Core.Repositories;
+using YetAnotherECommerce.Shared.Abstractions.Commands;
+using YetAnotherECommerce.Shared.Abstractions.Events;
+
+namespace YetAnotherECommerce.Modules.Products.Core.Commands
+{
+    public class UpdatePriceCommandHandler : ICommandHandler<UpdatePriceCommand>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IEventDispatcher _eventDispatcher;
+
+        public UpdatePriceCommandHandler(IProductRepository productRepository, IEventDispatcher eventDispatcher)
+        {
+            _productRepository = productRepository;
+            _eventDispatcher = eventDispatcher;
+        }
+
+        public async Task HandleAsync(UpdatePriceCommand command)
+        {
+            var product = await _productRepository.GetByIdAsync(command.ProductId);
+
+            if (product is null)
+                throw new ProductDoesNotExistException(command.ProductId);
+
+            product.UpdatePrice(command.Price);
+
+            await _productRepository.UpdateAsync(product);
+
+            await _eventDispatcher.PublishAsync(new PriceUpdated(command.ProductId, product.Price));
+        }
+    }
+}
--- tests/Modules/Products/YetAnotherECommerce.Modules.Products.UnitTests/Entities/ProductTests.cs
+++ tests/Modules/Products/YetAnotherECommerce.Modules.Products.UnitTests/Entities/ProductTests.cs
@@ -20,5 +20,17 @@ namespace YetAnotherECommerce.Modules.Products.UnitTests.Entities
             product.Quantity.Value.ShouldBe(quantity);
             product.Events.First().ShouldBeOfType<QuantityUpdated>();
         }
+
+        [Fact]
+        public void UpdatePrice_ShouldUpdateAndAddDomainEvent()
+        {
+            var product = Mock.Of<Product>();
+            var price = 10;
+
+            product.UpdatePrice(price);
+
+            product.Price.Value.ShouldBe(price);
+            product.Events.First().ShouldBeOfType<PriceUpdated>();
+        }
     }
 }
