194a631c71385eda4b84599b841dc758f9ebb43e
Damian Lubera <dmlubera@gmail.com>
2026-02-25 21:32:15 +0100

    Fix location parameter

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -50,7 +50,7 @@ resource "azurerm_linux_web_app" "web" {
 resource "azurerm_key_vault" "kv" {
   name                = "yetanotherecommerce-kv-${var.environment_name}"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.location
+  location            = azurerm_resource_group.rg.location
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
 }
