a15dd998c4a53613e7519dd0d6d6bd5b893648d3
Damian Lubera <dmlubera@gmail.com>
2026-02-25 21:45:46 +0100

    Use shorter name for resources

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,12 +17,12 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "rg" {
-  name     = "yetanotherecommerce-rg-${var.environment_name}"
+  name     = "yaecommerce-rg-${var.environment_name}"
   location = "West Europe"
 }
 
 resource "azurerm_service_plan" "asp" {
-  name                = "yetanotherecommerce-asp-${var.environment_name}"
+  name                = "yaecommerce-asp-${var.environment_name}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
@@ -30,7 +30,7 @@ resource "azurerm_service_plan" "asp" {
 }
 
 resource "azurerm_linux_web_app" "web" {
-  name                = "yetanotherecommerce-web-${var.environment_name}"
+  name                = "yaecommerce-web-${var.environment_name}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_service_plan.asp.location
   service_plan_id     = azurerm_service_plan.asp.id
@@ -48,7 +48,7 @@ resource "azurerm_linux_web_app" "web" {
 }
 
 resource "azurerm_key_vault" "kv" {
-  name                = "yetanotherecommerce-kv-${var.environment_name}"
+  name                = "yaecommerce-kv-${var.environment_name}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   tenant_id           = data.azurerm_client_config.current.tenant_id
