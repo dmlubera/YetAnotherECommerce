b22439b067f970df15648d5e25f624c082853015
Damian Lubera <dmlubera@gmail.com>
2026-03-23 22:46:01 +0100

    Fix resource name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -48,7 +48,7 @@ resource "azurerm_linux_web_app" "web" {
 }
 
 resource "azurerm_static_web_app" "swa" {
-  name                = "yeaecommerce-swa${var.environment_name}"
+  name                = "yeaecommerce-swa-${var.environment_name}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku_tier            = "Free"
